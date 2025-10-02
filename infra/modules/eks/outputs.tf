3e3702e1bb6e0c3c92280a458cfcb3babfbb92d5
Shoaib Shaikh <shoaibsshaikh1999@gmail.com>
2025-10-02 16:55:29 +0530

    Add cluster_name output

--- infra/modules/eks/outputs.tf
+++ infra/modules/eks/outputs.tf
@@ -2,6 +2,10 @@ output "cluster_id" {
   value = aws_eks_cluster.cluster.id
 }
 
+output "cluster_name" {
+  value = aws_eks_cluster.cluster.name
+}
+
 output "cluster_endpoint" {
   value = aws_eks_cluster.cluster.endpoint
 }
